2fb33294ffb5457988cfb7e1147466f925a79270
jsmio <jsmio@Msn.com>
2017-08-07 11:30:12 +0800

    修改

--- index.php
+++ index.php
@@ -1,50 +1,38 @@
 <?php get_header(); ?>
 <!-- nav end -->
 <!-- content srart -->
-                    </p>
-                    <span class="blog-bor">2015/10/9</span>
-                    <br><br><br><br><br><br><br>                
-                </div>
-            </div>
-      </li>
-      <li>
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/i/b2.jpg">
-            <div class="am-slider-desc blog-slider-desc">
-                <div class="blog-text-center blog-slider-con">
-                    <span><a href="" class="blog-color">Article &nbsp;</a></span>               
-                    <h1 class="blog-h-margin"><a href="">总在思考一句积极的话</a></h1>
-                    <p>那时候刚好下着雨，柏油路面湿冷冷的，还闪烁着青、黄、红颜色的灯火。
-                    </p>
-                    <span>2015/10/9</span>                
-                </div>
-            </div>
-      </li>
-      <li>
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/i/b3.jpg">
-            <div class="am-slider-desc blog-slider-desc">
-                <div class="blog-text-center blog-slider-con">
-                    <span><a href="" class="blog-color">Article &nbsp;</a></span>               
-                    <h1 class="blog-h-margin"><a href="">总在思考一句积极的话</a></h1>
-                    <p>修改，柏油路面湿冷冷的，还闪烁着青、黄、红颜色的灯火。
-                    </p>
-                    <span>2015/10/9</span>                
-                </div>
-            </div>
-      </li>
-      <li>
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/i/b2.jpg">
-            <div class="am-slider-desc blog-slider-desc">
-                <div class="blog-text-center blog-slider-con">
-                    <span><a href="" class="blog-color">Article &nbsp;</a></span>               
-                    <h1 class="blog-h-margin"><a href="">总在思考一句积极的话</a></h1>
-                    <p>showbar。
-                    </p>
-                    <span>2015/10/9</span>                
-                </div>
-            </div>
-      </li>
-    </ul>
-    </div>
+
+<div class="am-g am-g-fixed">
+ 
+<div class="am-show-md-up">
+	<div class="am-u-lg-6 am-g">
+<a rel="nofollow" target="_blank" href="//www.wpdaxue.com/go/weix" title="性价比最高的香港ASP、PHP主机提供商"><img src="//img.wpdaxue.com/2015/03/wx0329.jpg" width="728" height="90" alt="性价比最高的香港ASP、PHP主机提供商"></a>	
+	</div>
+	<div class="am-u-lg-6 am-g ">
+	<a rel="nofollow" target="_blank" href="https://www.wpdaxue.com/go/faisco2/" title="做网站，就上凡科建站"><img src="//img.wpdaxue.com/2017/06/fk01.png" width="560" height="70" alt="做网站，就上凡科建站"></a>	
+	</div>
+</div>  <!-- banner start -->
+<div class="am-u-md-8 am-u-sm-12 cat-box">
+  <div data-am-widget="slider" class="am-slider am-slider-b1" data-am-slider='{"controlNav:false","directionNav":false}' >
+	<ul class="am-slides">
+		<li>
+			<img src="http://s.amazeui.org/media/i/demos/bing-1.jpg">
+
+		</li>
+		<li>
+			<img src="http://s.amazeui.org/media/i/demos/bing-2.jpg">
+
+		</li>
+		<li>
+			<img src="http://s.amazeui.org/media/i/demos/bing-3.jpg">
+
+		</li>
+		<li>
+			<img src="http://s.amazeui.org/media/i/demos/bing-4.jpg">
+
+		</li>
+	</ul>
+</div>
 
 <!-- banner end -->
 <section class="cat-box recent-box recent-blog cat-box">
